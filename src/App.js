7bc14e9c545408a4f1b3137583cdbec5c1d7b0aa
warley mendes <wmlwarley@gmail.com>
2023-01-06 11:48:23 -0300

    Ajustes CSS

--- src/App.js
+++ src/App.js
@@ -9,21 +9,40 @@ function App() {
   return (
     <>
       < Header />
-    
-      <RedocStandalone
-          specUrl= {Doc}
-          options={{
-            nativeScrollbars: true,
-              theme: { 
-                colors: { 
-                  primary: { 
-                    main: '#004cff' 
-                  } 
-                } 
-              },
-          }}
-        />
-        
+
+          <RedocStandalone
+              specUrl= {Doc}
+              options={{
+                nativeScrollbars: true,
+                  theme: { 
+                    colors: { 
+                      primary: { 
+                        main: '#004cff' 
+                      } 
+                    } 
+                  },
+              }}
+            />
+
+      <style jsx>{`
+      .ejgidu {
+        padding: 0px 0;
+      }
+      .sc-hLBbgP.kzDJPW.api-info {
+        background: #f8f8f8;
+        width: 100%;
+        padding: 30px 80px 30px 40px;
+      }
+
+      .sc-hLBbgP.fWrHAO {
+        background: #f8f8f8;
+        width: 100%;
+        padding: 30px 80px 30px 40px;
+      }  
+
+
+
+      `}</style>
     </>
   );
 }
